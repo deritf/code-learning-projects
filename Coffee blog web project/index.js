d810fc32cf18c055e945389a0570e4eec672ea79
Derimán Tejera <deriman.tejera@gmail.com>
2024-06-20 19:49:59 +0100

    Subiendo los archivos locales hasta ahora

---
+++ Coffee blog web project/index.js
@@ -0,0 +1,12 @@
+const name = "perrito";
+
+/**
+ * ! Esto es un comentario clásico.
+ *
+ * ? Esto no es cun comentario clásico.
+ *
+ * Vaya vaya * cara
+ * *Caracola
+ *
+ * TODO: Esto es lo que debes hacer, A y B.
+ */
